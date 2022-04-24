9b9c11dc676151767e0d19d71f5cb6ab24987129
ashcruzzz <ashleycruzart@gmail.com>
2022-04-24 15:43:41 -0700

    updated weather icon

--- src/index.js
+++ src/index.js
@@ -23,22 +23,28 @@ function formatDate(timestamp) {
 
 function displayTemperature(response) {
   console.log(response.data);
-  console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind-speed");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", resposne.data.weather[0].description);
 }
 
 let apiKey = "fb963945219d8365844e016b8eda02af";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Las Vegas&appid=fb963945219d8365844e016b8eda02af&units=metric`;
+let city = "Las Vegas";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fb963945219d8365844e016b8eda02af&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
